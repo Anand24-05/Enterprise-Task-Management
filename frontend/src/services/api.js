8c8c48989921f41c691c77b5daaecb751732cbe4
Anand24-05 <anandnawle05@gmail.com>
2026-05-19 14:16:32 +0530

    api updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'https://enterprise-task-management.onrender.com',
+  baseURL: 'https://enterprise-task-management.onrender.com/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' }
 })
